4d3e53eb9417d3c33e0155325529489f3b1fe555
estan <elise.stankus@gmail.com>
2024-02-21 17:57:30 -0800

    imported header, footer, and app css file

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+
+import Footer from './components/Footer';
+
+import './styles/App.css';
 
 function App() {
 
   return (
     <>
-    
+
     </>
   )
 }
