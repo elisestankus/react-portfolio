f71a07ee7043c62c6fe54373ded4264dca29a36a
estan <elise.stankus@gmail.com>
2024-02-22 16:33:50 -0800

    set up router and added to render method

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -8,5 +8,29 @@ import Contact from './pages/Contact.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import Resume from './pages/Resume.jsx';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <AboutMe />,
+            },
+            {
+                path: '/Contact',
+                element: <Contact />,
+            },
+            {
+                path: '/Portfolio',
+                element: <Portfolio />,
+            },
+            {
+                path: '/Resume',
+                element: <Resume />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
