59cf13b3e18362bf02d92b01e58df695d37f55ab
estan <elise.stankus@gmail.com>
2024-02-22 16:28:51 -0800

    imported pages

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Contact from './pages/Contact.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
