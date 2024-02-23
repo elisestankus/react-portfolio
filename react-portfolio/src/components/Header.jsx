2ccd8c807d87e0fdb0a3bdb4fbf9be68eff7c3d8
estan <elise.stankus@gmail.com>
2024-02-22 16:35:36 -0800

    imported navigation component and nested inside header

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import '../styles/Header.css';
+import Navigation from './Navigation';
 
 function Header() {
     return (
         <header className="header">
             <h1>Elise Stankus</h1>
+            < Navigation />
         </header>
     );
 }
